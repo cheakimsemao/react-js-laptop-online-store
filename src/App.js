8f007e17d27ff716a8130d8fb5f2a33c14ef2302
Seanghai <=>
2020-09-07 12:04:44 +0700

    install react-bootstrap && react-icon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
-
-
+import { Button } from 'react-bootstrap';
+import { FaBeer } from 'react-icons/fa';
+import { BsFillChatDotsFill } from "react-icons/bs";
 function App() {
     return (
         <div>
             <Header />
             <Footer />
+            <h3> Lets go for a <FaBeer />? </h3>
+            <h3> Lets go for a <BsFillChatDotsFill />? </h3>
         </div>
     );
 }
