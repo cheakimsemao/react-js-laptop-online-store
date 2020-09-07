7bcc46c521f667ccbd589073e309967b3acaa9dc
cheangfousuy <thecounterfeitcat@gmail.com>
2020-09-07 21:46:17 +0700

    Modification to header and slight change to index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+
 
 function App() {
     return (
         <div>
             <Header />
+            <SignIn />
+            <SignUp />
             <Footer />
         </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import SignIn from './SignIn'
-import SignUp from './SignUp'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 const Header = () => {
     return (
-        <div>
-            This is the HeaderPart.
-        </div>
+        <>
+            {['Home', 'About', 'Brand', 'Accessories', 'Search', 'Account', 'Cart'].map(
+            (variant, idx) => (
+            <DropdownButton
+                as={ButtonGroup}
+                key={idx}
+                id={`dropdown-variants-${variant}`}
+                variant={variant.toLowerCase()}
+                title={variant}
+            >
+                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                <Dropdown.Item eventKey="3" active>
+                Active Item
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+            </DropdownButton>
+            ),
+        )}
+        </>
     )
 };
 
