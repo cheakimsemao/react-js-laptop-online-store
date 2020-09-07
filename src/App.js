1b977c24c9a4611523cf29d6646973d9efd130f4
cheangfousuy <thecounterfeitcat@gmail.com>
2020-09-07 11:52:29 +0700

    basic changes to files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 function App() {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <Header />
+            <Footer />
         </div>
     );
 }
