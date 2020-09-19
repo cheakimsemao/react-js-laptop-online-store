814f2b98e35b08b4166cf86763f8ba089469cad3
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2020-09-20 00:24:06 +0700

    Fixed About

--- src/components/About.js
+++ src/components/About.js
@@ -14,50 +14,44 @@ const About = () => {
             <div>
                 <h1 className='text-center about-title'>About Us</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod suspendisse
-                    placerat est varius id vitae nulla. Morbi diam gravida risus sodales eget
-                    vestibulum. Nunc molestie mattis in pretium elementum id morbi id. Pharetra ac
-                    adipiscing aliquet sagittis egestas.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod suspendisse placerat est
+                    varius id vitae nulla. Morbi diam gravida risus sodales eget vestibulum. Nunc molestie
+                    mattis in pretium elementum id morbi id. Pharetra ac adipiscing aliquet sagittis egestas.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ornare sollicitudin
-                    feugiat nulla aliquet lorem. Arcu fermentum mattis lectus quis netus semper
-                    donec orci, a. Sed gravida nec, fermentum mi. Tortor phasellus id sed nullam
-                    condimentum cursus. Tellus egestas tempor mauris sed. In commodo, fusce interdum
-                    ipsum malesuada etiam id eros. In amet, integer velit odio sed fermentum morbi
-                    sed vitae. Purus orci scelerisque semper accumsan bibendum a tristique et. Nulla
-                    adipiscing mauris velit venenatis nibh placerat vitae eget a. Libero sed tortor
-                    neque, aliquam tortor ipsum posuere. Viverra donec bibendum velit in sodales
-                    maecenas dolor id.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ornare sollicitudin feugiat
+                    nulla aliquet lorem. Arcu fermentum mattis lectus quis netus semper donec orci, a. Sed
+                    gravida nec, fermentum mi. Tortor phasellus id sed nullam condimentum cursus. Tellus
+                    egestas tempor mauris sed. In commodo, fusce interdum ipsum malesuada etiam id eros. In
+                    amet, integer velit odio sed fermentum morbi sed vitae. Purus orci scelerisque semper
+                    accumsan bibendum a tristique et. Nulla adipiscing mauris velit venenatis nibh placerat
+                    vitae eget a. Libero sed tortor neque, aliquam tortor ipsum posuere. Viverra donec
+                    bibendum velit in sodales maecenas dolor id.
                 </p>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ornare sollicitudin
-                    feugiat nulla aliquet lorem. Arcu fermentum mattis lectus quis netus semper
-                    donec orci, a. Sed gravida nec, fermentum mi. Tortor phasellus id sed nullam
-                    condimentum cursus. Tellus egestas tempor mauris sed. In commodo, fusce interdum
-                    ipsum malesuada etiam id eros. In amet, integer velit odio sed fermentum morbi
-                    sed vitae. Purus orci scelerisque semper accumsan bibendum a tristique et. Nulla
-                    adipiscing mauris velit venenatis nibh placerat vitae eget a.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ornare sollicitudin feugiat
+                    nulla aliquet lorem. Arcu fermentum mattis lectus quis netus semper donec orci, a. Sed
+                    gravida nec, fermentum mi. Tortor phasellus id sed nullam condimentum cursus. Tellus
+                    egestas tempor mauris sed. In commodo, fusce interdum ipsum malesuada etiam id eros. In
+                    amet, integer velit odio sed fermentum morbi sed vitae. Purus orci scelerisque semper
+                    accumsan bibendum a tristique et. Nulla adipiscing mauris velit venenatis nibh placerat
+                    vitae eget a.
                 </p>
             </div>
             <div className='text-center'>
                 <h1 className='team-title'>Our Team</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod suspendisse
-                    placerat est varius id vitae nulla. Morbi diam gravida risus sodales eget
-                    vestibulum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Euismod suspendisse placerat est
+                    varius id vitae nulla. Morbi diam gravida risus sodales eget vestibulum.
                 </p>
             </div>
             <Container className='card-container'>
                 <Row id='team'>
-                    <Col>
+                    <Col lg={3}>
                         <Card className='team-card '>
                             <Card.Body>
-                                <div className='position-relative rounded-circle overflow-hidden mx-auto custom-circle-image'>
-                                    <img
-                                        className='w-100 h-100'
-                                        src='https://scontent.fpnh3-1.fna.fbcdn.net/v/t1.0-9/116666648_1570223706461040_8934943181573800174_o.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=ybVyNpGswtsAX-0uMot&_nc_ht=scontent.fpnh3-1.fna&oh=4e9076f34362a7e220bdc446ae867881&oe=5F8633E0'
-                                    ></img>
+                                <div className='position-relative overflow-hidden mx-auto team-profile'>
+                                    <img src='https://scontent.fpnh3-1.fna.fbcdn.net/v/t1.0-9/116666648_1570223706461040_8934943181573800174_o.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=ybVyNpGswtsAX-0uMot&_nc_ht=scontent.fpnh3-1.fna&oh=4e9076f34362a7e220bdc446ae867881&oe=5F8633E0'></img>
                                 </div>
                                 <Card.Title className='card-name'>Cheang Suy Fou</Card.Title>
                                 <Card.Text className='card-location'>
@@ -80,14 +74,11 @@ const About = () => {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col>
+                    <Col lg={3}>
                         <Card className='team-card '>
                             <Card.Body>
-                                <div className='position-relative rounded-circle overflow-hidden mx-auto custom-circle-image'>
-                                    <img
-                                        className='w-100 h-100'
-                                        src='https://scontent.fpnh3-1.fna.fbcdn.net/v/t1.0-9/17903666_1953359964897194_5081674949640058810_n.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=F0DkYjrkETIAX-KIDSa&_nc_ht=scontent.fpnh3-1.fna&oh=267a716b3af44a529ffe0405003ebabd&oe=5F869840'
-                                    ></img>
+                                <div className='position-relative overflow-hidden mx-auto team-profile'>
+                                    <img src='https://scontent.fpnh3-1.fna.fbcdn.net/v/t1.0-9/17903666_1953359964897194_5081674949640058810_n.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=F0DkYjrkETIAX-KIDSa&_nc_ht=scontent.fpnh3-1.fna&oh=267a716b3af44a529ffe0405003ebabd&oe=5F869840'></img>
                                 </div>
                                 <Card.Title className='card-name'>Seanghai Heng</Card.Title>
                                 <Card.Text className='card-location'>
@@ -110,14 +101,11 @@ const About = () => {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col>
+                    <Col lg={3}>
                         <Card className='team-card'>
                             <Card.Body>
-                                <div className='position-relative rounded-circle overflow-hidden mx-auto custom-circle-image'>
-                                    <img
-                                        className='w-100 h-100'
-                                        src='https://scontent.fpnh3-1.fna.fbcdn.net/v/t1.0-9/65241776_1088465818208068_5493561444177805312_o.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=0tchq1k2rVMAX-Z16EV&_nc_ht=scontent.fpnh3-1.fna&oh=06f45ed87aea74823ea4d35130656b4d&oe=5F8989C0'
-                                    ></img>
+                                <div className='position-relative overflow-hidden mx-auto team-profile'>
+                                    <img src='https://scontent.fpnh3-1.fna.fbcdn.net/v/t1.0-9/65241776_1088465818208068_5493561444177805312_o.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=0tchq1k2rVMAX-Z16EV&_nc_ht=scontent.fpnh3-1.fna&oh=06f45ed87aea74823ea4d35130656b4d&oe=5F8989C0'></img>
                                 </div>
                                 <Card.Title className='card-name'>Cheakimhok Mao</Card.Title>
                                 <Card.Text className='card-location'>
@@ -140,14 +128,11 @@ const About = () => {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col>
-                        <Card className='team-card '>
+                    <Col lg={3}>
+                        <Card className='team-card'>
                             <Card.Body>
-                                <div className='position-relative rounded-circle overflow-hidden mx-auto custom-circle-image'>
-                                    <img
-                                        className='w-100 h-100'
-                                        src='https://scontent.fpnh6-1.fna.fbcdn.net/v/t1.0-9/65968500_862318104161355_718931629329874944_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=qSVuAONCXXcAX86QjPY&_nc_ht=scontent.fpnh6-1.fna&oh=0cd87d93fcd8c03a787f13e36def5eb1&oe=5F89ACCC'
-                                    ></img>
+                                <div className='position-relative overflow-hidden mx-auto team-profile'>
+                                    <img src='https://scontent.fpnh6-1.fna.fbcdn.net/v/t1.0-9/65968500_862318104161355_718931629329874944_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=qSVuAONCXXcAX86QjPY&_nc_ht=scontent.fpnh6-1.fna&oh=0cd87d93fcd8c03a787f13e36def5eb1&oe=5F89ACCC'></img>
                                 </div>
                                 <Card.Title className='card-name'>Cheakimse Mao</Card.Title>
                                 <Card.Text className='card-location'>
@@ -172,7 +157,7 @@ const About = () => {
                     </Col>
                 </Row>
                 <Row id='contact'>
-                    <Col sm={5} className='contact-padding-1'>
+                    <Col lg={5} className='contact-padding-1'>
                         <h1 className='mb-5'>Contact Info</h1>
                         <div id='contact-margin'>
                             <h5 id='contact-info'>
@@ -204,7 +189,7 @@ const About = () => {
                             </p>
                         </div>
                     </Col>
-                    <Col sm={7} className='contact-padding-2'>
+                    <Col lg={7} className='contact-padding-2'>
                         <h1 className='text-center mb-5'>Get In Touch</h1>
                         <Form id='submit-form'>
                             <div className='input-field'>
@@ -229,11 +214,7 @@ const About = () => {
 
                             <div className='input-field'>
                                 <Form.Group controlId='exampleForm.ControlTextarea1'>
-                                    <Form.Control
-                                        as='textarea'
-                                        rows='3'
-                                        placeholder='Your message'
-                                    />
+                                    <Form.Control as='textarea' rows='3' placeholder='Your message' />
                                     <MdMessage className='input-icon' />
                                 </Form.Group>
                             </div>
@@ -250,8 +231,7 @@ const About = () => {
                     frameborder='0'
                     allowfullscreen=''
                     aria-hidden='false'
-                    tabindex='0'
-                ></iframe>
+                    tabindex='0'></iframe>
             </Container>
         </div>
     );
