a6bd8972acbe3c6db4ce154fcd636e2e898a0e8b
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2020-10-03 09:55:47 +0700

    Arranged Files

--- src/components/brands/Laptop.js
+++ src/components/brands/Laptop.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useLocation} from 'react';
+import React, { useState, useEffect, useLocation } from 'react';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Carousel from 'react-bootstrap/Carousel';
@@ -6,7 +6,7 @@ import NativeSelect from '@material-ui/core/NativeSelect';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Pagination from 'react-bootstrap/Pagination';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Link } from 'react-router-dom';
 import Rating from '@material-ui/lab/Rating';
 import Box from '@material-ui/core/Box';
 import Macbook1 from '../../assets/images/others/laptops/Macbook.png';
@@ -25,24 +25,21 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-function Laptop(props){
+function Laptop(props) {
     const classes = useStyles();
-    console.log(props)
-    const [data, setData]= useState([])
-    useEffect(()=>{
-        axios.get(`http://localhost:3000${props.match.path}/products`)
-        .then(res=> 
-            {                
-                      console.log(res)
-                    setData(res.data)
-            }
-        )
-    },[props.match.path])
+    console.log(props);
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get(`http://localhost:3000${props.match.path}/products`).then((res) => {
+            console.log(res);
+            setData(res.data);
+        });
+    }, [props.match.path]);
 
     const grid = data.map((item) => (
-        <div className={'product-items index'}>    
-         <Link to={`/Brands/${item.brandId}/${item.id}`}>      
-            <img src={item.imageURL} alt='mac' width="300px" height="200px" />
+        <div className={'product-items index'}>
+            <Link to={`/Brands/${item.brandId}/${item.id}`}>
+                <img src={item.imageURL} alt='mac' width='300px' height='200px' />
             </Link>
             <p class='product-items-title'>{item.name}</p>
             <div className={classes.root}>
@@ -53,25 +50,20 @@ function Laptop(props){
             <p class='price'>{item.price}</p>
         </div>
     ));
-    const productName = data.slice(0,1).map((item)=>(
-        <>
-            {item.brandId}
-        </>
 
-    ))
+    const productName = data.slice(0, 1).map((item) => <>{item.brandId}</>);
 
     return (
-        
         <>
             <div className='breadcrumbs'>
                 <p>
-                    <Link to="/" style={{"text-decoration": "none", "color": "black"}} >
-                    Home/
+                    <Link to='/' style={{ 'text-decoration': 'none', color: 'black' }}>
+                        Home/
                     </Link>
-                    <Link to="/Brands" style={{"text-decoration": "none", "color": "black"}} >
+                    <Link to='/Brands' style={{ 'text-decoration': 'none', color: 'black' }}>
                         Brands/
                     </Link>
-                   <span>{productName}</span>
+                    <span>{productName}</span>
                 </p>
             </div>
             <Carousel id='carousel'>
@@ -99,7 +91,7 @@ function Laptop(props){
             </Carousel>
             <div id='products'>
                 <div className='review-title'>
-    <span> {productName} Products</span>
+                    <span> {productName} Products</span>
                     <hr />
                 </div>
                 <div className='sorted-by'>
@@ -126,6 +118,6 @@ function Laptop(props){
             </Pagination>
         </>
     );
-};
+}
 
 export default Laptop;
