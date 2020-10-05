dbd8bb523857c8c1f96fc6f038d2ed8720c2d0d8
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2020-10-05 15:12:21 +0700

    Fixed Product List

--- src/Display.js
+++ src/Display.js
@@ -1,61 +1,57 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import ClipLoader from "react-spinners/ClipLoader";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 function DisplayTest(match) {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState({ loading: true });
-  console.log(match);
-  useEffect(() => {
-    axios
-      .get(`http://localhost:3000/products/${match.match.params.id}`)
-      .then((res) => {
-        console.log(res);
-        setData(res.data);
-        setLoading({ loading: false });
-      })
-      .catch((err) => {
-        if (err.response) {
-          setLoading({ loading: false });
-        }
-      });
-  }, []);
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState({ loading: true });
 
-  const goBack = () => {
-    window.history.back();
-  };
-  console.log(loading.loading);
-  return (
-    <>
-      {data.id ?
-       (
-        <div>
-          <Link to={`/Brands${data.url}`}>Back</Link>
-          <br></br>
-          <img height="200px" width="300px" src={data.imageURL} alt="pic"></img>
-          <h1>Name: {data.name}</h1>
-          <h1>Description: {data.description}</h1>
-          <h1>Price: {data.price}</h1>
-        </div>
-      ) 
-      : (
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3000/products/${match.match.params.id}`)
+            .then((res) => {
+                setData(res.data);
+                setLoading({ loading: false });
+            })
+            .catch((err) => {
+                if (err.response) {
+                    setLoading({ loading: false });
+                }
+            });
+    }, []);
+
+    const goBack = () => {
+        window.history.back();
+    };
+
+    return (
         <>
-          <ClipLoader size={150} color={"#123abc"} loading={loading.loading} />
-
-          {loading.loading === false ? (
-            <>
-              <Link onClick={goBack}>Go Back</Link>
-              <br></br>
-              Sorry, no data available
-            </>
-          ) : (
-            ""
-          )}
+            {data.id ? (
+                <div>
+                    <Link to={`/Brands${data.url}`}>Back</Link>
+                    <br></br>
+                    <img height='200px' width='300px' src={data.imageURL} alt='pic'></img>
+                    <h1>Name: {data.name}</h1>
+                    <h1>Description: {data.description}</h1>
+                    <h1>Price: {data.price}</h1>
+                </div>
+            ) : (
+                <>
+                    <ClipLoader size={150} color={'#123abc'} loading={loading.loading} />
+                    {loading.loading === false ? (
+                        <>
+                            <Link onClick={goBack}>Go Back</Link>
+                            <br></br>
+                            Sorry, no data available
+                        </>
+                    ) : (
+                        ''
+                    )}
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 }
 
 export default DisplayTest;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './Header';
 import Footer from './Footer';
@@ -9,10 +9,9 @@ import Home from './Home';
 import About from './About';
 import Brands from './Brands';
 import Checkout from './Checkout';
-import {BrowserRouter as Router, Route,Switch, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 // brands
-
 import ProductDetails from './ProductDetails';
 // accessories
 import Keyboard from './accessories/Keyboard';
@@ -25,25 +24,20 @@ import AccessoryDetails from './AccessoryDetails';
 
 import Display from '../Display';
 import Laptop from './brands/Laptop';
+import { ImTerminal } from 'react-icons/im';
 
 const Main = () => {
+    const [data, setData] = useState([]);
 
-    const [data, setData]= useState([])
-    useEffect(()=>{
-        axios.get(`http://localhost:3000/brands`)
-        .then(res=> 
-            {                
-                      console.log(res)
-                    setData(res.data)
-            }
-        )
-    },[])
+    useEffect(() => {
+        axios.get(`http://localhost:3000/brands`).then((res) => {
+            setData(res.data);
+        });
+    }, []);
 
-    const product = data.map((item)=>(
-        
-        <Route path={`/Brands/${item.name}/:id`} component={Display} />
-        
-    ))
+    const productDetails = data.map((item) => (
+        <Route path={`/brands/${item.name}/:id`} component={Display} />
+    ));
 
     return (
         <div>
@@ -53,12 +47,11 @@ const Main = () => {
                 <Route path={'/about'} component={About} />
                 <Route exact path={'/brands'} component={Brands} />
                 <Route path={'/Brands/:id'} exact component={Laptop} />
+                {productDetails}
                 <Route path={'/signin'} component={SignIn} />
                 <Route path={'/signup'} component={SignUp} />
                 <Route path={'/cart'} exact component={Cart} />
                 <Route path={'/cart/checkout'} component={Checkout} />
-                {product}
-
                 <Route path={'/accessories/keyboard'} component={Keyboard} />
                 <Route path={'/accessories/mouse'} component={Mouse} />
                 <Route path={'/accessories/headset'} component={Headset} />
@@ -67,7 +60,6 @@ const Main = () => {
                 <Route path={'/accessories/adaptor'} component={Adaptor} />
                 <Route path={'/brands/product-details'} component={ProductDetails} />
                 <Route path={'/brands/accessory-details'} component={AccessoryDetails} />
-
             </Switch>
             <Footer />
         </div>
