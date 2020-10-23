3ad7bdb3feaec056dc420c6d75de8091375d6453
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2020-10-23 09:31:41 +0700

    Added Links in Brands Page

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 
 import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import Apple from '../assets/images/others/brands/Apple.png';
 import Razer from '../assets/images/others/brands/Razer.png';
@@ -33,110 +34,138 @@ const Brands = () => {
                 <Container className='brands-container'>
                     <Row id='team' className='brand-item'>
                         <Col lg={4}>
-                            <Card className='brand-apple'>
-                                <Card.Body>
-                                    <img src={Apple}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Apple'>
+                                <Card className='brand-apple'>
+                                    <Card.Body>
+                                        <img src={Apple}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                         <Col lg={4}>
-                            <Card className='brand-razer'>
-                                <Card.Body>
-                                    <img src={Razer}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Razer'>
+                                <Card className='brand-razer'>
+                                    <Card.Body>
+                                        <img src={Razer}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                         <Col lg={4}>
-                            <Card className='brand-microsoft'>
-                                <Card.Body>
-                                    <img src={Microsoft}></img>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row id='team' className='brand-item'>
-                        <Col lg={4}>
-                            <Card className='brand-google'>
-                                <Card.Body>
-                                    <img src={Google}></img>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4}>
-                            <Card className='brand-alienware'>
-                                <Card.Body>
-                                    <img src={Alienware}></img>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4}>
-                            <Card className='brand-samsung'>
-                                <Card.Body>
-                                    <img src={Samsung}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Microsoft'>
+                                <Card className='brand-microsoft'>
+                                    <Card.Body>
+                                        <img src={Microsoft}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                     </Row>
                     <Row id='team' className='brand-item'>
                         <Col lg={4}>
-                            <Card className='brand-dell'>
-                                <Card.Body>
-                                    <img src={Dell}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Google'>
+                                <Card className='brand-google'>
+                                    <Card.Body>
+                                        <img src={Google}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                         <Col lg={4}>
-                            <Card className='brand-asus'>
-                                <Card.Body>
-                                    <img src={Asus}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Alienware'>
+                                <Card className='brand-alienware'>
+                                    <Card.Body>
+                                        <img src={Alienware}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                         <Col lg={4}>
-                            <Card className='brand-sony'>
-                                <Card.Body>
-                                    <img src={Sony}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Samsung'>
+                                <Card className='brand-samsung'>
+                                    <Card.Body>
+                                        <img src={Samsung}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                     </Row>
                     <Row id='team' className='brand-item'>
                         <Col lg={4}>
-                            <Card className='brand-hp'>
-                                <Card.Body>
-                                    <img src={Hp}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Dell'>
+                                <Card className='brand-dell'>
+                                    <Card.Body>
+                                        <img src={Dell}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                         <Col lg={4}>
-                            <Card className='brand-lenovo'>
-                                <Card.Body>
-                                    <img src={Lenovo}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Asus'>
+                                <Card className='brand-asus'>
+                                    <Card.Body>
+                                        <img src={Asus}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                         <Col lg={4}>
-                            <Card className='brand-msi'>
-                                <Card.Body>
-                                    <img src={Msi}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Sony'>
+                                <Card className='brand-sony'>
+                                    <Card.Body>
+                                        <img src={Sony}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
+                        </Col>
+                    </Row>
+                    <Row id='team' className='brand-item'>
+                        <Col lg={4}>
+                            <Link to='/brands/HP'>
+                                <Card className='brand-hp'>
+                                    <Card.Body>
+                                        <img src={Hp}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
+                        </Col>
+                        <Col lg={4}>
+                            <Link to='/brands/Lenovo'>
+                                <Card className='brand-lenovo'>
+                                    <Card.Body>
+                                        <img src={Lenovo}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
+                        </Col>
+                        <Col lg={4}>
+                            <Link to='/brands/MSI'>
+                                <Card className='brand-msi'>
+                                    <Card.Body>
+                                        <img src={Msi}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                     </Row>
                     <Row id='team' className='brand-item' style={{ marginBottom: '200px' }}>
                         <Col lg={4}>
-                            <Card className='brand-huawei'>
-                                <Card.Body>
-                                    <img src={Huawei}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Huawei'>
+                                <Card className='brand-huawei'>
+                                    <Card.Body>
+                                        <img src={Huawei}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                         <Col lg={4}>
-                            <Card className='brand-xiaomi'>
-                                <Card.Body>
-                                    <img src={Xiaomi}></img>
-                                </Card.Body>
-                            </Card>
+                            <Link to='/brands/Xiaomi'>
+                                <Card className='brand-xiaomi'>
+                                    <Card.Body>
+                                        <img src={Xiaomi}></img>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
                         </Col>
                     </Row>
                 </Container>
